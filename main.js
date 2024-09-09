49fc109c1ea1eaded43dcb4a7c2cab3a5538f9b1
adelacruz <albertodelacruz96@gmail.com>
2024-09-09 18:21:34 +0200

    feat: improved resume page structure

--- main.js
+++ main.js
@@ -1,12 +1,100 @@
 class ProjectCard extends HTMLElement{
+    _style = `
+        <style>
+            .css-logo-color {
+                background-color:  #264de4;
+            }
+
+            .js-logo-color {
+                background-color: #F0DB4F;
+            }
+
+            .html-logo-color {
+                background-color: #F16529;
+            }
+
+            .card {
+                padding: 17px;
+                display: flex;
+                flex-direction: column;
+
+                width: 300px;
+                height: 300px;
+
+                align-items: center;
+                gap: 7px;
+
+                text-decoration: none;
+                background-color: #fff;
+
+                box-shadow: #9D958F 0px 8px 24px;
+
+                overflow: hidden;
+                transition: transform 0.3s ease-in-out;
+
+            }
+
+            .card .website-detail{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+
+            .card .tags {   
+                display: flex;
+                gap: 7px;
+
+                flex-wrap: wrap;
+                height: 0;
+                overflow: hidden;
+                transition: height 0.3s ease-in-out;
+            }
+
+            .tag {
+                font-size: 12px;
+                border: 4px black;
+                border-radius: 7px;
+
+                padding: 7px;
+
+                color: #fff;
+            }
+
+            .card:hover .tags {
+                height: 30px;
+            }
+
+            .card .img-wrapper{
+                width: 100%;
+                height: 100%;
+            }
+
+            .card img {
+                width: 100%;
+                height: 100%; 
+                object-fit: cover;                
+            }
+
+            .card .website-title {    
+                font-size: 20px;
+                font-weight: 400;
+            }
+
+            .card .description {    
+                font-size: 14px;
+                color: gray;
+            }
+        </style>`;
+
     constructor(){
         super();
+        this.attachShadow({ mode: "open" });
         this._href = this.getAttribute("href") || "";
         this._imgRef = this.getAttribute("img-ref") || "";
         this._imgAlt = this.getAttribute("img-alt") || "";
         this._cardTitle = this.getAttribute("card-title") || "";
         this._cardDescr = this.getAttribute("card-descr") || "";
-        this._tags = this.getAttribute("tags") || [];
+        this._tags = this.getAttribute("tags") ? this.getAttribute("tags").split(",") : [];
     }
 
     connectedCallback(){
@@ -25,39 +113,27 @@ class ProjectCard extends HTMLElement{
                 color = "js-logo-color";
             }
 
-            tags.push(`<span class="tag ${color}">HTML5</span>`);
+            tags.push(`<span class="tag ${color}">${tag}</span>`);
         }
 
-        this.innerHTML = `
-            <a href="${this._href}" target="_blank" class="website-wrapper">
-                <img src="${this._imgRef}" alt="${this._imgAlt}">
+        this.shadowRoot.innerHTML = `
+            ${this._style}
+            <a href="${this._href}" target="_blank" class="card">
+                <div class="img-wrapper">
+                    <img src="${this._imgRef}" alt="${this._imgAlt}">
+                </div>
                 <div class="website-detail">
                     <span class="website-title">${this._cardTitle}</span>
                     <p class="description">${this._cardTitle}</p>
-                    <span class="website-tags-section non-active-section">
+                    <span class="website-tags-section">
                         <div class="tags">
-                            <span class="tag html-logo-color">HTML5</span>
-                            <span class="tag css-logo-color">CSS3</span>
-                            <span class="tag js-logo-color">JavaScript</span>
+                            ${tags.join("")}
                         </div>
                     </span>
                 </div>
             </a>
         `;
 
-        this._handleMouseHover();
-    }
-
-    _handleMouseHover(){
-        this.addEventListener("mouseover", () => {
-            const tags = this.getElementsByClassName('website-tags-section');
-            tags[0].classList.remove('non-active-section');
-        });
-
-        this.addEventListener("mouseout", () => {
-            const tags = this.getElementsByClassName('website-tags-section');
-            tags[0].classList.add('non-active-section');
-        });
     }
 }
 
